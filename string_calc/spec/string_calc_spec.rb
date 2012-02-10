12e29d399bdebe9acb6e7d7a7b52098f57b6022b
Rob Romito <rob@robertromito.com>
2012-02-09 22:24:59 -0500

    Awesome string calc kata powered by Rob x 2

--- string_calc/spec/string_calc_spec.rb
+++ string_calc/spec/string_calc_spec.rb
@@ -1,60 +1,53 @@
 class StringCalc
 
-  DEFAULT_DELIMETER = ","
-  DELIMETER_OVERRIDE_INDICATOR = "//"
+  DELIMITER = ','
   
-  def calculate(operand_string)
-    delimeter, operands = parse_operand_string(operand_string)
-    validate_operands(operands)
-    return operands.inject(0){|sum, i| sum += i.to_i}
+  def self.string_calc(operands_string)
+    operands = normalize_delimiter_in_operands(operands_string)
+    return  operands.split(DELIMITER).inject(0){|sum, i| sum += i.to_i }
   end
 
-  def validate_operands(operands)
-    negative_operands = operands.select {|i|i.to_i < 0}
-    raise ArgumentError, "negatives not allowed: #{negative_operands.join(",")}" if negative_operands.count > 0
+  def self.normalize_delimiter_in_operands(operands)
+    alt_delimiter = ["\n"]
+
+    operands.gsub(/^\/\/(.)\n/, '')
+    alt_delimiter << $1 unless $1.nil?
+    
+    alt_delimiter.each {|d| operands.gsub!(d, DELIMITER) }
+    return operands
   end
 
-  def parse_operand_string(operand_string)
-    return DEFAULT_DELIMETER, operand_string.split(DEFAULT_DELIMETER) unless operand_string.start_with?(DELIMETER_OVERRIDE_INDICATOR)
-    delimeter = operand_string[2].chr
-    return delimeter, operand_string.partition("\n").last.split(delimeter)
-  end
 end
 
-describe "String Calculator" do
+describe "StringCalculator" do  
 
-  before(:each) do
-    @calc = StringCalc.new
+  it "returns zero when given and empty string" do 
+    StringCalc.string_calc("").should eql 0
+  end
+
+  it "returns operand when passed a single operand" do 
+    StringCalc.string_calc("3").should eql 3
+    StringCalc.string_calc("100").should eql 100
+    StringCalc.string_calc("0").should eql 0
+  end
+
+  it "returns sum of two numbers when passed a string delimited with a comma" do
+    StringCalc.string_calc("3,4").should eql 7
+    StringCalc.string_calc("100,100").should eql 200
+  end
+
+  it "returns the sum of any number of operands" do
+    StringCalc.string_calc("1,2,3,4,5").should eql 15
+    StringCalc.string_calc("20,20,20,20,20,20").should eql 120
+  end
+
+  it "returns the sum of numbers when given a string delimited with either a comma and/or newline" do
+    StringCalc.string_calc("1\n2,3,4\n5").should eql 15
+  end
+
+  it "returns the sum of numbers when given a string with a defined delimiter" do
+    StringCalc.string_calc("//;\n1;2;3").should eql 6
+    StringCalc.string_calc("//;\n1;2;3\n4,5;6").should eql 21
   end
   
-  it "returns zero for an empty string" do
-    @calc.calculate("").should eql 0
-  end
-
-  it "returns the single operand when given one operand" do
-    @calc.calculate("1").should eql 1
-  end
-
-  it "returns the sum of the operands when given 2 operands" do
-    @calc.calculate("1,2").should eql 3
-  end
-
-  it "returns the sum of the operands when given more than 2 operands" do
-    @calc.calculate("1,2,3").should eql 6
-    @calc.calculate("9,9,9,9,9,9,9").should eql 63
-  end
-
-  it "returns the sum of the operands when \n is used as a separator" do
-    @calc.calculate("1\n,2,3").should eql 6
-  end
-
-  it "supports changing the delimeter" do
-    @calc.calculate("//;\n1;2").should eql 3
-    @calc.calculate("//*\n10*20*30*40").should eql 100
-  end
-
-  it "hurls when given a negative operand" do
-    expect { @calc.calculate("-1,1") }.should raise_error(ArgumentError, "negatives not allowed: -1")
-  end
-  
-end    
+end
