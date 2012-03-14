9b4040ec38e7b9a3c92be53c7ae6a76dd4783f4f
Rob Romito <rob@robertromito.com>
2012-03-13 20:26:55 -0400

    Step 14

--- rubykoans/koans/about_objects.rb
+++ rubykoans/koans/about_objects.rb
@@ -31,9 +31,9 @@ class AboutObjects < EdgeCase::Koan
   end
 
   def test_some_system_objects_always_have_the_same_id
-    assert_equal __, false.object_id
-    assert_equal __, true.object_id
-    assert_equal __, nil.object_id
+    assert_equal 0, false.object_id
+    assert_equal 2, true.object_id
+    assert_equal 4, nil.object_id
   end
 
   def test_small_integers_have_fixed_ids
